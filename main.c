2c38ec86e671fcb15734ae39c79a449f9bf36353
ksv1986 <ksv0x07c2@gmail.com>
2023-04-04 23:03:32 +0200

    Add invisible window
    The program will use only tray icon popup menu,
    but it's simpler to use window handle to process messages.
    
    No need to show and paint this window though.

--- main.c
+++ main.c
@@ -1,13 +1,61 @@
 #include <windows.h>
 
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
+{
+    switch (umsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProcW(hwnd, umsg, wparam, lparam);
+}
+
 int WINAPI wWinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE hprev, _In_ PWSTR argv, _In_ int show)
 {
-    UNREFERENCED_PARAMETER(hinst);
     UNREFERENCED_PARAMETER(hprev);
     UNREFERENCED_PARAMETER(argv);
     UNREFERENCED_PARAMETER(show);
 
-    return 0;
+    // The program will use only tray icon popup menu,
+    // but it's simpler to use window handle to process messages.
+    // No need to show and paint this window though
+    LPCWCH dummy = L"DummyWindow";
+    WNDCLASSEX wcex = {
+        .cbSize = sizeof(wcex),
+        .lpszClassName = dummy,
+        .style = CS_HREDRAW | CS_VREDRAW,
+        .lpfnWndProc = WndProc,
+        .hInstance = hinst,
+    };
+    // window class will be unregistered on exit, no need to call UnregisterClass()
+    RegisterClassExW(&wcex);
+
+    // Create the window.
+    HWND hwnd = CreateWindowExW(
+        0,                  // Optional window styles.
+        dummy,              // Window class
+        dummy,              // Window text
+        WS_OVERLAPPED,      // Window style
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,               // Parent window
+        NULL,               // Menu
+        hinst,              // Instance handle
+        NULL                // Additional application data
+    );
+    if (hwnd == NULL)
+        return 1;
+
+    MSG msg[1];
+    while (GetMessageW(msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(msg);
+        DispatchMessageW(msg);
+    }
+
+    return (int)msg->wParam;
 }
 
 int __stdcall
