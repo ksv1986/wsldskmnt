7cb6326333c96c3e8983338e073052287278ed71
ksv1986 <ksv0x07c2@gmail.com>
2023-04-09 17:04:23 +0200

    List physical disks
    And add menu to copy its path.
    
    In case of any error occurs, corresponding message
    will be added to menu.

--- main.c
+++ main.c
@@ -2,10 +2,14 @@
 #include "shared.h"
 
 #include <windows.h>
+#include <objbase.h>
+#include <shlwapi.h>
+#include <strsafe.h>
 
 enum {
     APP_NOTIFY = WM_APP + 1, // Tray icon notification callback message
     MENU_EXIT = 40001,
+    MENU_COPY = 40100,
 };
 // Tray icon will be identified by guid
 static const GUID GUID_NOTIFY = {
@@ -76,14 +80,49 @@ static void showContextMenu(HWND hwnd)
     TrackPopupMenuEx(getMenu(hwnd), flags, pt.x, pt.y, hwnd, NULL);
 }
 
+static void copyToClipboard(HGLOBAL hdst)
+{
+    if (!OpenClipboard(NULL))
+        return;
+
+    EmptyClipboard();
+    SetClipboardData(CF_UNICODETEXT, hdst);
+    CloseClipboard();
+}
+
+static void onCopyClicked(HWND hwnd, DWORD i)
+{
+    state* st = getState(hwnd);
+    disk_info* disk = getDisk(st, i);
+
+    const int cch = lstrlenW(disk->path) + 1;
+    HGLOBAL hdst = GlobalAlloc(GMEM_MOVEABLE, cch * sizeof(WCHAR));
+    if (!hdst)
+        return;
+
+    PWSTR dst = (LPWSTR)GlobalLock(hdst);
+    if (dst) {
+        StringCchCopyW(dst, cch, disk->path);
+        GlobalUnlock(hdst);
+
+        copyToClipboard(hdst);
+    }
+    GlobalFree(hdst);
+}
+
 static LRESULT onMenuCommand(HWND hwnd, WPARAM wparam, LPARAM lparam)
 {
-    switch (LOWORD(wparam))
+    const UINT cmd = LOWORD(wparam);
+    switch (cmd)
     {
     case MENU_EXIT:
         DestroyWindow(hwnd);
         return 0;
     default:
+        if (cmd >= MENU_COPY && cmd < MENU_COPY + MAX_DISKS) {
+            onCopyClicked(hwnd, cmd - MENU_COPY);
+            return 0;
+        }
         return DefWindowProc(hwnd, WM_COMMAND, wparam, lparam);
     }
 }
@@ -101,8 +140,35 @@ static LRESULT onTrayCallback(HWND hwnd, WPARAM wparam, LPARAM lparam)
     return 0;
 }
 
+static void appendError(HMENU menu, err_desc* e)
+{
+    AppendMenuW(menu, MF_STRING | MF_DISABLED, 0, e->title);
+    AppendMenuW(menu, MF_STRING | MF_DISABLED, 0, e->text);
+}
+
+static void createDiskMenu(HMENU parent, DWORD i, disk_info* disk)
+{
+    HMENU menu = CreatePopupMenu();
+    if (disk->e->error)
+        appendError(menu, disk->e);
+    else {
+        AppendMenuW(menu, MF_STRING, MENU_COPY + i, L"&Copy device path");
+    }
+    WCHAR text[256] = L"";
+    wnsprintfW(text, ARRAYSIZE(text), L"&%u: %s %u parts",
+        disk->index, disk->model, disk->n_parts);
+    text[ARRAYSIZE(text) - 1] = 0;
+    AppendMenuW(parent, MF_STRING | MF_POPUP, (UINT_PTR)menu, text);
+}
+
 static void createDisksMenu(state* st)
 {
+    if (st->e->error)
+        appendError(st->menu, st->e);
+
+    for (DWORD i = 0; i < st->n_disks; i++)
+        createDiskMenu(st->menu, i, getDisk(st, i));
+
     AppendMenuW(st->menu, MF_STRING, MENU_EXIT, L"&Exit");
 }
 
@@ -110,8 +176,18 @@ static LRESULT onCreate(HWND hwnd, LPARAM lparam)
 {
     LPCREATESTRUCTW cs = (LPCREATESTRUCTW)lparam;
     state* st = cs->lpCreateParams;
-
     setState(hwnd, st);
+
+    switch (CoInitializeEx(NULL, COINIT_MULTITHREADED)) {
+    case S_OK:
+    case S_FALSE:
+        break;
+    default:
+        return GetLastError();
+    }
+
+    listDisks(st);
+
     st->menu = CreatePopupMenu();
     if (!st->menu)
         return GetLastError(); // without menu program is useless
@@ -129,6 +205,9 @@ static void onDestroy(HWND hwnd)
 
     removeTrayIcon(hwnd);
     DestroyMenu(st->menu);
+    resetDisks(st);
+
+    CoUninitialize();
 }
 
 static LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
--- shared.h
+++ shared.h
@@ -2,10 +2,51 @@
 
 #include <windows.h>
 
+// Dynamic allocations are good if we need lots of memory.
+// But this a simple program. It has simple needs.
+#define MAX_DISKS 32
+#define MAX_DRIVE_PATH 24
+
+// Container for readable error message with a title
+typedef struct err_desc {
+    PCWCH title;
+    PWCHAR text;
+    DWORD error; // windows error code
+} err_desc;
+
+#define ERRINIT() { .text = L"" }
+
+typedef struct disk_info {
+    err_desc e[1];
+    DWORD index;
+    PWCHAR model;
+    WCHAR path[MAX_DRIVE_PATH];
+    DWORD n_parts;
+} disk_info;
+
 // Global program state
 typedef struct state {
     HINSTANCE hinst;
     HWND hwnd;
     HMENU menu;
+
+    err_desc e[1]; // if there was a problem to enumerate disks
+    DWORD n_disks;
+    disk_info disk[MAX_DISKS];
 } state;
 
+// Free resources used by error
+void resetErr(err_desc* e);
+
+
+DWORD setError(err_desc* e, PCWCH title);
+
+// Enumerate physical disks and fill disk_info array.
+// Returns 0 on success and GetLastError() on failure.
+HRESULT listDisks(state* st);
+void resetDisks(state* st);
+
+static __inline disk_info* getDisk(state* st, DWORD i)
+{
+    return &st->disk[i];
+}
